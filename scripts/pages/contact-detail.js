141ad14e47d6d1111ebef2ad7800750223560b23
Claudia Berrios <cberriosledesma@gmail.com>
2022-02-26 16:14:21 -0500

    first

--- scripts/pages/contact-detail.js
+++ scripts/pages/contact-detail.js
@@ -1,5 +1,10 @@
 function render() {
-  return `
+  return `<section>
+  <div class="container-detail">
+    <h1 class="heading title--sm">Contact Detail</h1>
+    <p1 class="content content--sm blue">Logout</p1>
+  </div>
+</section>
   `
 
 }
